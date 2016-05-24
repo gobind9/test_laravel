bc9d1a27e22ff4096bfecc359c968d3a80e7ddfd
Gobind Kumar Chauhan <gobind.chauhan@asergis.in>
2016-05-24 13:50:21 +0530

    worked on order search

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -5,6 +5,26 @@
     <div class="row">
       <div class="col-md-12">
         <h1>Order Management </h1>
+		{!! Form::open(['url' => 'order', 'method'=>'get', 'class'=>'form-inline']) !!}
+            <div class="form-group">
+				Product Name {!! Form::text('q', isset($q) ? $q : null, ['class'=>'form-control']) !!}
+				Customer {!! Form::select('customer', $customers, isset($cust) ? $cust : 0, ['class' => 'form-control']) !!}
+				From <div class='input-group date'>
+				<input type='text' class="form-control" data-provide="datepicker" date-date-formate="YY-MM-DD" id="from" name="from" value="{{ $fromDate }}" />
+				<span class="input-group-addon">
+				<span class="glyphicon glyphicon-calendar"></span>
+				</span>
+				</div>
+				To <div class='input-group date'>
+				<input type='text' class="form-control" data-provide="datepicker" date-date-formate="YY-MM-DD" id="to" name="to" value="{{ $toDate }}" />
+				<span class="input-group-addon">
+				<span class="glyphicon glyphicon-calendar"></span>
+				</span>
+				</div>
+            </div>
+
+          {!! Form::submit('Search', ['class'=>'btn btn-primary']) !!}
+		{!! Form::close() !!}
         <table class="table table-striped table-bordered table-hover">
           <thead>
             <tr class="bg-info">
@@ -26,8 +46,17 @@
             @endforeach
           </tbody>
         </table>
-       
+       {!! $orders->links() !!}
       </div>
     </div>
   </div>
+  	<script type="text/javascript">
+		$(function () {
+		$('#from').datepicker({
+			format:'yyyy-mm-dd'});
+			$('#to').datepicker({
+			format:'yyyy-mm-dd'});
+		});
+	</script>
+
 @endsection
